aa3723beac4288063f530d0cdcc6102fdb17e912
jiuxing <jiuxing.lzh@alibaba-inc.com>
2021-02-05 10:04:52 +0800

    初始化构建

--- springboot/course/src/main/java/com/chase/course/CourseApplication.java
+++ springboot/course/src/main/java/com/chase/course/CourseApplication.java
@@ -2,8 +2,9 @@ package com.chase.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CourseApplication {
 
 	public static void main(String[] args) {
---
+++ springboot/course/src/main/java/com/chase/course/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.chase.course.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiuxing.lzh
+ * Date: 2021/2/5
+ **/
+@RestController
+@Configuration
+public class HelloController {
+
+}
